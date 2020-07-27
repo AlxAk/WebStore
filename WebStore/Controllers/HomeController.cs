d6201ed9f006901c23834f7c3894c841d98d51e9
AlxAk <my.name-akulov@yandex.ru>
2020-07-27 08:59:10 +0500

    Статическа html страница

---
+++ WebStore/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Conflict("Controller-Home, Action-Index");
+        }
+        public IActionResult Some()
+        {
+            return Conflict("Controller-Home, Action-Some");
+        }
+    }
+}
